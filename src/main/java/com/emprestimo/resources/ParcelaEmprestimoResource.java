ba73de7b2a86c1dd4ed1f28fb69a7f5070ba09f5
Edson Sales <edsonsales99@gmail.com>
2021-12-31 17:53:31 -0500

    Mundano o @RequestMapping do recurso ParcelaEmprestimo

--- src/main/java/com/emprestimo/resources/ParcelaEmprestimoResource.java
+++ src/main/java/com/emprestimo/resources/ParcelaEmprestimoResource.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/pessoas")
+@RequestMapping(value = "/parcelaemprestimos")
 public class ParcelaEmprestimoResource {
 
     /**
